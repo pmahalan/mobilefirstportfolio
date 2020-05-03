4092608e1c6bd280cf51421417d93e3e6238a409
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-05-03 16:43:56 -0400

    pushing up everything after having clicked-and-dragged the contentss into the repo folder from my desktop

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+// By extending the React.Component class, ContacatForm inherits functionality from it
+class ContactForm extends React.Component {
+  
+// Setting the initial state of the ContactForm component
+constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  };
+
+  submitForm(ev) {
+    console.log('Form submitted!')
+    const form = ev.target
+    console.log(form)
+  };
+
+  // The render method returns the JSX that should be rendered
+  render() {
+    return (
+
+    <div className="contactme">
+        <h3 className="contactheading">Contact Me</h3>
+
+        <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/xqkdnlga"
+        method="POST"
+      >
+
+        <label> Name </label>
+        <input type="text" 
+        className="itybityformfield" 
+        name="name"
+        placeholder="Your name"
+        style={{width: '35%'}}
+            // value={this.state.fname}
+            // onChange={e => this.setState({ fname: e.target.value })}
+        />
+
+        &emsp;
+
+        <label>Email </label>
+        <input type="email"
+        className="itybityformfield" 
+        name="_replyto" 
+        placeholder="Your email address"
+        style={{width: '35%'}}
+            // value={this.state.email}
+            // onChange={e => this.setState({ email: e.target.value })}
+        />
+
+        <br></br>
+        <br></br>
+
+        <label>Message </label>
+        <br></br>
+        <textarea
+        className="itybityformfield" 
+        name="message"
+        placeholder="Your message"
+        style={{width: '90%', height: '120px'}}
+            // onChange={e => this.setState({ message: e.target.value })}
+            // value={this.state.message}
+        ></textarea>
+
+        <br></br>
+        <br></br>
+
+        <input type="submit" 
+        // onClick={e => this.handleFormSubmit(e)} 
+        className="submitbutton" 
+        value="Send"
+        label="send"
+
+        />
+
+        <br></br>
+
+        </form>
+    </div>
+
+        );
+    };
+};
+
+export default ContactForm
---
+++ client/src/components/MainPage/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+import ContactForm from "../../components/Contact";
+
+function MainPage(props) {
+    return (
+<div className="App">
+<div className="App-header">
+
+        <h1 className="hithere">Priya Mahalanobis</h1>
+
+        <img className="mepic" src={require('../../../src/var3.png')}></img>
+
+        <p className="blurbing"> Welcome to my website! In February 2020, I earned my Full Stack Web Development <a className="mylinks" href="https://credly.com/credit/23192550">certificate</a> from the UCF Coding Boot Camp. </p>
+        <p className="blurbing">  I am excited to further develop my skillsets as a junior developer in the workplace; in any technical endeavor, I strive to be resourceful in finding solutions to challenges. </p>
+        <p className="blurbing">  Alongside my bootcamp certificate, my <a className="mylinks" href="https://www.ucf.edu/degree/digital-media-ma/">Master of Arts in Digital Media</a> has provided me with an awareness of theoretical, philosophical, and creative outlooks on technology and the web. </p>
+        <p className="relocation"> Open to relocation. </p>
+
+
+        <h2 className="littleheading">Skills</h2>
+
+        <p className="maintext"> HTML </p>
+        <p className="maintext"> CSS </p>
+        <p className="maintext"> JavaScript </p>
+        <p className="maintext"> React.js </p>
+        <p className="maintext"> MySQL </p>
+        <p className="maintext"> MongoDB </p>
+        <p className="maintext"> Express.js </p>
+        <p className="maintext"> jQuery </p>
+        <p className="maintext"> Node.js </p>
+
+        <h2 className="littleheading">Contact</h2>
+
+        <p className="maintext"> <a className="mylinks" href="https://www.linkedin.com/in/priya-mahalanobis-75a103189">LinkedIn</a> </p>
+        <p className="maintext"> <a className="mylinks" href="https://github.com/pmahalan">Git Hub</a> </p>
+        <p className="maintext"> <a className="mylinks" href="Priya_M_Resume.pdf">Resume</a></p>
+        <p className="maintext"> <a className="mylinks" href="mailto:pmahalan@knights.ucf.edu">Email</a></p>
+    </div>    
+
+<h2 className="recentprojects">Recent Projects</h2>
+
+<div className="asingleproject">
+    <h2 className="projecttitle1"> GLITTER </h2>
+    
+    <img className="projectpic" src={require('../../../src/new1.png')}></img>
+    <p className="projectdescription"
+        // style={{fontsize: '22px'}}
+    
+    > A MERN application with functionality to create, read and delete 
+        notes pertaining to travel goals as part of a fun and inspirational personal dashboard. </p>
+    <p className="projectlink"
+        // style={{fontsize: '22px'}}
+
+    > Deployed Application: 
+        <a className="mylinks2" href="https://arcane-retreat-34096.herokuapp.com/"
+        // style={{fontsize: '22px'}}
+        
+        > https://arcane-retreat-34096.herokuapp.com/</a> </p>
+    <p className="projectlink"> GitHub: 
+        <a className="mylinks2" href="https://github.com/pmahalan/GLITTER"
+            // style={{fontsize: '22px'}}
+
+        > https://github.com/pmahalan/GLITTER </a> </p>
+</div>
+
+<div className="asingleproject2">
+    <h2 className="projecttitle"> Text Tracker </h2>
+    <img className="projectpic" src={require('../../../src/new2.png')}></img>
+    <p className="projectdescription"> A full-stack application utilizing a webhook API to automate the storage of user information into a MySQL database. Provides users with CRUD functionality on a mobile-first UI.   </p>
+    <p className="projectlink"> Deployed Application: 
+    <a className="mylinks2" href="https://protected-thicket-21207.herokuapp.com/"> https://protected-thicket-21207.herokuapp.com/</a> </p>
+    <p className="projectlink"> GitHub: 
+    <a className="mylinks2" href="https://github.com/pmahalan/Project_2"> https://github.com/pmahalan/Project_2 </a> </p>
+</div>
+
+<div className="lastlistedproject">
+    <h2 className="projecttitle"> Profile Generator </h2>
+    <img className="projectpic" src={require('../../../src/new4.png')}></img>
+    <p className="projectdescription"> A command-line application which renders an HTML page and a PDF document of a person’s GitHub profile information, through the use of the GitHub API as well as node.js packages such as Inquirer and Electron.
+        My first project using Node.js.
+    </p>
+    <p className="projectlink"> GitHub: 
+        <a className="mylinks2" href="https://github.com/pmahalan/Node.js-Generator"> https://github.com/pmahalan/Node.js-Generator</a> </p>
+</div>
+
+<ContactForm></ContactForm>
+
+ </div>
+    );
+}
+
+export default MainPage;
